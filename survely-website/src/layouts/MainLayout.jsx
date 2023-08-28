46eb01db91ca0d25ebb7fc10d1174e8dfab5a835
ju-dev-17 <jahidudd65@gmail.com>
2023-08-28 15:56:53 +0200

    Edit Height of Page

--- survely-website/src/layouts/MainLayout.jsx
+++ survely-website/src/layouts/MainLayout.jsx
@@ -4,8 +4,8 @@ import Navbar from "../components/ui/Navbar.jsx";
 
 export default function MainLayout() {
     return (
-        <main className="w-full min-h-screen bg-background text-text flex flex-col items-center">
-            <div className="w-full h-1/6">
+        <main className="w-full h-screen bg-background text-text flex flex-col items-center">
+            <div className="w-full">
                 <Navbar />
             </div>
             <div className="w-full h-5/6 flex flex-col sm:flex-row justify-around p-8 md:p-0">
