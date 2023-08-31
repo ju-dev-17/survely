d8cdd71aeaac3923cbacafdb51c4c02bebe42189
ju-dev-17 <jahidudd65@gmail.com>
2023-08-31 11:38:17 +0200

    Implement delete email from list functionality

--- survely-website/src/components/create-form/EmailTag.jsx
+++ survely-website/src/components/create-form/EmailTag.jsx
@@ -1,5 +1,33 @@
 /* eslint-disable react/prop-types */
+import { useRecoilState } from "recoil";
+import toast from "react-hot-toast";
+
+import { surveyEmailListState } from "../../recoil/atom.js";
+import close from "../../assets/close-outline.svg";
 
 export default function EmailTag({ email }) {
-    return <span className="p-2 rounded-lg bg-accent text-white text-sm">{email}</span>;
+    const [surveyEmailList, setSurveyEmailList] = useRecoilState(surveyEmailListState);
+
+    const handleDelete = () => {
+        const updatedSurveyEmailList = surveyEmailList.filter(
+            surveyEmail => surveyEmail !== email
+        );
+
+        setSurveyEmailList(updatedSurveyEmailList);
+        toast.success("E-Mail successfully removed from list")
+    }
+
+    return (
+        <span className="p-2 rounded-lg bg-accent text-white text-sm flex">
+            {email}
+            <img
+                className="cursor-pointer"
+                onClick={handleDelete}
+                src={close}
+                alt="Delete E-Mail"
+                width={24}
+                height={24}
+            />
+        </span>
+    );
 }
