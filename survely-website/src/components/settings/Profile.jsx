72465e6b5359d21b43bed37b209ed1a9d224d4fe
ju-dev-17 <jahidudd65@gmail.com>
2023-09-01 20:44:22 +0200

    Finish profile view

--- survely-website/src/components/settings/Profile.jsx
+++ survely-website/src/components/settings/Profile.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import Breadcrumb from "../ui/Breadcrumb.jsx";
 import profile from "../../assets/profile.svg";
 import upload from "../../assets/cloud-upload-outline.svg";
 
 export default function Profile() {
+    const [profileData, setProfileData] = useState({
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@survely.com"
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setProfileData((prevProfileData) => ({
+            ...prevProfileData,
+            [name]: value,
+        }));
+    };
+
     return (
         <div className="w-full h-full flex flex-col gap-5">
             <Breadcrumb section="Account settings" view="Profile" />
             <h1 className="text-3xl font-semibold">Profile</h1>
-            <div className="border-t border-x-0 flex flex-col py-8">
+            <div className="border-y border-x-0 flex flex-col gap-10 py-8">
                 <div className="flex items-center gap-5">
                     <img
                         className="rounded-full"
                         src={profile}
                         alt="Profile Picture"
-                        width={64}
-                        height={64}
+                        width={55}
+                        height={55}
                     />
                     <div className="flex flex-col gap-3">
                         <span className="font-semibold">Profile Picture</span>
@@ -33,6 +51,51 @@ export default function Profile() {
                                 Remove
                             </button>
                         </div>
+                    </div>
+                </div>
+                <div className="space-y-5">
+                    <div className="flex gap-5">
+                        <div className="flex flex-col gap-1 w-full">
+                            <label className="text-accent" htmlFor="firstName">First Name</label>
+                            <input
+                                id="firstName"
+                                className="rounded-lg py-2 px-4 border-2 focus:outline-none w-full"
+                                type="text"
+                                name="firstName"
+                                placeholder={`${profileData.firstName} *`}
+                                value={profileData.firstName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-1 w-full">
+                            <label className="text-accent" htmlFor="lastName">Last Name</label>
+                            <input
+                                id="lastName"
+                                className="rounded-lg py-2 px-4 border-2 focus:outline-none w-full"
+                                type="text"
+                                name="lastName"
+                                placeholder={`${profileData.lastName} *`}
+                                value={profileData.lastName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-1 w-full">
+                        <label className="text-accent" htmlFor="lastName">Primary E-Mail Address</label>
+                        <input
+                            id="lastName"
+                            className="rounded-lg py-2 px-4 border-2 focus:outline-none w-full"
+                            type="text"
+                            name="lastName"
+                            value={profileData.email}
+                            disabled
+                        />
+                        <Link
+                            className="underline text-accent"
+                            to="/settings/email"
+                        >
+                            Change your email address
+                        </Link>
                     </div>
                 </div>
             </div>
